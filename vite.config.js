bcb5bce42fac61935838db8f7419b889b08129ff
Frank Thoeny <teknofrank@gmail.com>
2026-03-02 19:02:48 -0800

    add HMR to the config (WSL)

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    // Required for HMR to work inside Docker or remote hosts
+    // host: '0.0.0.0',
+    // Use polling in environments where native file watching is unreliable (e.g., WSL2, network file systems)
+    watch: {
+      usePolling: true,
+    },
+    // Specific HMR options
+    hmr: {
+      host: '127.0.0.1',
+      // Other HMR specific options can go here
+    },
+  },
 })
