54d693c7c93f5e9b2541ad1c2dbce1d645d8e729
Frank Thoeny <teknofrank@gmail.com>
2026-03-04 18:32:07 -0800

    adjusted the spacing

--- src/components/pages/CaseStudiesPage.jsx
+++ src/components/pages/CaseStudiesPage.jsx
@@ -5,7 +5,7 @@ export default function CaseStudiesPage({ isDark }) {
   return (
     <Suspense
       fallback={
-        <div className="mt-40 text-center">Loading case studies...</div>
+        <div className="mt-10 text-center">Loading case studies...</div>
       }
     >
       <CaseStudiesSection isDark={isDark} />
